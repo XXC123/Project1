f6248b7761cc00283b99bfadd5639e2f667d7288
rmit-s3580240-yiqing-ding <s3580240@student.rmit.edu.au>
2019-10-02 08:02:50 +0000

    img reduce

--- app/Models/img.php
+++ app/Models/img.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Response;
 use Redirect,View;
 use Input,DB;
+use Image;
 class img extends Model
 {
 
@@ -21,23 +22,40 @@ class img extends Model
 
         $realPath = $file -> getRealPath();
 
-        $entension = $file -> getClientOriginalExtension();
+        $extension = $file -> getClientOriginalExtension();
 
-        if(!in_array($entension,$rule)){
+        if(!in_array($extension,$rule)){
 
             return 'format jpg,png,gif';
         }
 
         $mimeTye = $file -> getMimeType();
 
-        $newName = md5(date("Y-m-d H:i:s").$clientName).".".$entension;
+        $newName = md5(date("Y-m-d H:i:s").$clientName).".".$extension;
         $path = $file -> move($url_path,$newName);
 
                 $namePath = $url_path.'/'.$newName;
 
+                 if ($extension != 'gif') {
+                    $this->reduceSize($url_path.'/'.$newName);
+        }
+
                 return $namePath;
         // return ['name' => $newName,'path' => $namePath];
         }
+    }
+     public function reduceSize($newName) {
+
+        $image = Image::make($newName);
+
+         $image->resize(300, null, function($constraint) {
+
+           $constraint->aspectRatio();
+
+            $constraint->upsize();
+        });
+
+        $image->save();
     }
 
 }
